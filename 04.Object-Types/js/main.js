66531d09049cb797ec58929ab4218b307c0d11b0
parvez-prottoy <parvezahmedprottoy.web@gmail.com>
2025-02-11 10:07:44 +0600

    added objects types

---
+++ 04.Object-Types/js/main.js
@@ -0,0 +1,8 @@
+"use strict";
+console.log("Hello World!");
+// Object Types
+const car = {
+    name: "Toyota",
+    model: "Corolla",
+    year: 2009,
+};
---
+++ 04.Object-Types/ts/main.ts
@@ -0,0 +1,7 @@
+console.log("Hello World!");
+// Object Types
+const car: { name: string; model: string; year: number; mileage?: number } = {
+  name: "Toyota",
+  model: "Corolla",
+  year: 2009,
+};
