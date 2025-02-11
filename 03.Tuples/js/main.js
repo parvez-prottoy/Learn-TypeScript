54958dcfee5e695eae964b59cf4c5a1de764c2fc
parvez-prottoy <parvezahmedprottoy.web@gmail.com>
2025-02-11 09:56:40 +0600

    added arrays tuples

---
+++ 03.Tuples/js/main.js
@@ -0,0 +1,8 @@
+"use strict";
+console.log("Hello World!");
+// Typed Arrays
+// define our tuple
+let ourTuple;
+// initialize correctly
+// ourTuple = [5, "TypeScript", true]; // No error
+// ourTuple = [true, 5, "TypeScript"]; // Error
---
+++ 03.Tuples/ts/main.ts
@@ -0,0 +1,8 @@
+console.log("Hello World!");
+
+// Typed Arrays
+// define our tuple
+let ourTuple: [number, string, boolean];
+// initialize correctly
+// ourTuple = [5, "TypeScript", true]; // No error
+// ourTuple = [true, 5, "TypeScript"]; // Error
