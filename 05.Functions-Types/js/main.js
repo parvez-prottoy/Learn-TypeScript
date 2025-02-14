e0bb6ac1232b597ccb0f3d871589442501ebc3ad
parvez-prottoy <parvezahmedprottoy.web@gmail.com>
2025-02-14 15:39:42 +0600

    added function signature

--- 05.Functions-Types/js/main.js
+++ 05.Functions-Types/js/main.js
@@ -7,3 +7,10 @@ const addFun = (a, b, c = 0, e) => {
     return a + b + c;
 };
 addFun(10, 20, 0, { name: "parvez" });
+// Function Signatures
+let userDetails; // signature of function
+userDetails = (id, user) => {
+    console.log(`Hello, I'm ${user.name} and age is ${user.age}`);
+    return `Hello, I'm ${user.name} and age is ${user.age}`;
+};
+userDetails(102, { name: "Parvez", age: 30 });
--- 05.Functions-Types/ts/main.ts
+++ 05.Functions-Types/ts/main.ts
@@ -7,3 +7,25 @@ const addFun = (a: number, b: number, c: number = 0, e?: object) => {
   return a + b + c;
 };
 addFun(10, 20, 0, { name: "parvez" });
+
+// Function Signatures
+
+let userDetails: (
+  id: string | number,
+  user: {
+    name: string;
+    age: number;
+  }
+) => string; // signature of function
+
+userDetails = (
+  id: string | number,
+  user: {
+    name: string;
+    age: number;
+  }
+) => {
+  console.log(`Hello, I'm ${user.name} and age is ${user.age}`);
+  return `Hello, I'm ${user.name} and age is ${user.age}`;
+};
+userDetails(102, { name: "Parvez", age: 30 });
